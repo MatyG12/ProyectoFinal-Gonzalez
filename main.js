21a9363da140533efd1dc1a0c291a7137dd3b4b0
matygonzalez <mg987720@gmail.com>
2023-09-02 23:50:48 -0300

    sweet alert

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ let lista = document.getElementById("carrito-cartel")
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
+Swal.fire({
+    title: 'Registrate a nuestra página para obtener todas las novedades',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Aceptar',
+    showLoaderOnConfirm: true,
+    preConfirm: (login) => {
+      return fetch(`//api.github.com/users/${login}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        })
+        .catch(error => {
+          Swal.showValidationMessage(
+            `Request failed: ${error}`
+          )
+        })
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value.login}'s avatar`,
+        imageUrl: result.value.avatar_url
+      })
+    }
+  })
+
 function mostrarProductos() {
     fetch(json)
       .then((response) => {
